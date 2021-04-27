2fb62184644c5734d6938da670a18623f28662bf
Luc Saunders <luc.saunders@terazo.com>
2021-04-26 23:49:21 -0400

    init

---
+++ functions/sms-opt-out-tracking.js
@@ -0,0 +1,79 @@
+/* global exports, require, console, process, Twilio */
+'use strict';
+
+// import { config, SQS } from 'aws-sdk';
+const AWS = require('aws-sdk');
+
+export async function handler(context, event, callback) {
+  const payload = JSON.parse(event.Payload);
+  console.log(payload.error_code);
+  console.log(payload.resource_sid);
+
+  const client = context.getTwilioClient();
+  let stop_sms_data = {};
+
+  try {
+    // check for STOP-sms warning
+    if(payload.error_code=="80901"){
+        console.log("80901 error");
+        stop_sms_data = await client.messages(payload.resource_sid)
+            .fetch()
+    
+        console.log(JSON.stringify(stop_sms_data));
+    } else {
+        callback(null,"");
+    }
+
+    // set up stop-sms data tracking and AWS SQS call with aws-sdk
+    // await config.update({
+    //   accessKeyId: process.env.AWS_KEY,
+    //   secretAccessKey: process.env.AWS_SECRET
+    // });
+    await AWS.config.update({
+      accessKeyId: process.env.AWS_KEY,
+      secretAccessKey: process.env.AWS_SECRET
+  });
+  
+    // let sqs = new SQS({ region: 'us-east-2' });
+    let sqs = new AWS.SQS({ region: 'us-east-2' });
+
+
+    const res = new Twilio.Response();
+        res.appendHeader("Access-Control-Allow-Origin", "*");
+        res.appendHeader("Access-Control-Allow-Methods", "OPTIONS POST");
+        res.appendHeader("Content-Type", "application/json");
+        res.appendHeader("Access-Control-Allow-Headers", "Content-Type");
+    let response = { response: true };
+    
+    //TODO: scrub phone number from e16 "+"?
+    let stop_sms_body = JSON.stringify(
+      {
+      PhoneNumber: stop_sms_data.from,
+      StopDate: stop_sms_data.dateUpdated
+      }
+    )
+
+    let sendParameters = {
+        MessageBody: stop_sms_body,
+        QueueUrl: process.env.SMS_STOP_URL
+    };
+
+    // let sendParameters = {
+    //     MessageBody: JSON.stringify(stop_sms_data),
+    //     QueueUrl: SMS_STOP_URL
+    // };
+    
+    await sqs.sendMessage(sendParameters, (err, resp) => {
+        console.log(err);
+        console.log(resp);
+        response.return = resp;
+        response.error = err;
+        res.setBody(response);
+        callback(null, res);
+    });
+
+  } catch(error) {
+      console.log(error);
+      callback(error);
+  }
+}
